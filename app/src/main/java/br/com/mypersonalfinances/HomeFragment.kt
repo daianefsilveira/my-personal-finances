4f473df0a8b8ec3e8e45c3072a08e998d93ace3c
dsilveira <daiane.silveira@foursys.com.br>
2023-01-05 21:51:08 -0300

    setup project

--- app/src/main/java/br/com/mypersonalfinances/HomeFragment.kt
+++ app/src/main/java/br/com/mypersonalfinances/HomeFragment.kt
@@ -5,14 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import br.com.mypersonalfinances.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
-}
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupButtons()
+    }
+
+    private fun setupButtons() {
+        binding.btnNewTransaction.setOnClickListener {
+            callTransactions()
+        }
+    }
+
+        fun callTransactions() {
+            findNavController().navigate(R.id.action_homeFragment_to_addTransactionFragment)
+        }
+    }
