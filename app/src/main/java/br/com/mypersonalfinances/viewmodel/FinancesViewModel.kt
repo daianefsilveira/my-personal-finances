ff3a25f63c9b01bc5e17e1b7701b47eece8c3ef7
daia_ <daiane.pguni@gmail.com>
2023-01-12 17:07:36 -0300

    feat: implementing viewmodel

---
+++ app/src/main/java/br/com/mypersonalfinances/viewmodel/FinancesViewModel.kt
@@ -0,0 +1,44 @@
+package br.com.mypersonalfinances.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.*
+import br.com.mypersonalfinances.data.local.FinancesRepository
+import br.com.mypersonalfinances.model.Balance
+import br.com.mypersonalfinances.model.Transaction
+import kotlinx.coroutines.launch
+
+class FinancesViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository = FinancesRepository(application)
+    var transactionList = MutableLiveData<List<Transaction>>()
+    var balance = MutableLiveData<Balance>()
+
+    fun updateList() {
+        viewModelScope.launch {
+            transactionList.value = repository.getAll()
+            balance.value = repository.totalBalance(transactionList.value!!)
+        }
+    }
+
+    fun add(transaction: Transaction) {
+        viewModelScope.launch {
+            repository.insert(transaction)
+            updateList()
+        }
+    }
+
+    fun remove(position: Int) {
+        viewModelScope.launch {
+            val transaction = transactionList.value!![position]
+            repository.delete(transaction.id.toString())
+            updateList()
+        }
+    }
+
+    class FinancesViewModelFactory(private val application: Application) :
+        ViewModelProvider.AndroidViewModelFactory(application) {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T =
+            FinancesViewModel(application) as T
+    }
+}
