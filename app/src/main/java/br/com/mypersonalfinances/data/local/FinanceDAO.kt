8f2aacd72a28a21a254a6b43677bdd4056ed6d8c
daia_ <daiane.pguni@gmail.com>
2023-01-12 14:49:03 -0300

    feat: creating DAO

---
+++ app/src/main/java/br/com/mypersonalfinances/data/local/FinanceDAO.kt
@@ -0,0 +1,19 @@
+package br.com.mypersonalfinances.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import br.com.mypersonalfinances.model.Transaction
+
+
+@Dao
+interface FinanceDAO {
+    @Insert
+    suspend fun insert(transaction: Transaction)
+
+    @Query("DELETE FROM transaction_table WHERE id = :id")
+    suspend fun delete(id: String)
+
+    @Query("SELECT * FROM transaction_table")
+    suspend fun getAll(): List<Transaction>
+}
