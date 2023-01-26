94d2bce65c0d03d2ea84f2330c354fc225db1a4d
daia_ <daiane.pguni@gmail.com>
2023-01-26 20:35:50 -0300

    feat: updating icons from mipmap folder, setting spalsh screen

--- app/src/main/java/br/com/mypersonalfinances/MainActivity.kt
+++ app/src/main/java/br/com/mypersonalfinances/MainActivity.kt
@@ -3,6 +3,7 @@ package br.com.mypersonalfinances
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import br.com.mypersonalfinances.databinding.ActivityMainBinding
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
